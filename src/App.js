90795545607d8d84e383c035773e0a262893109d
Joao Bogo <joaoluciano9812@gmail.com>
2023-06-20 11:18:50 -0300

    u

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from "react";
-import "./index.css";
-import { Route, Routes } from "react-router-dom";
-import Provider from "./context/Provider";
-import Header from "./components/newport/Header/Header";
-import Projects from "./components/newport/Projects/Projects";
-import Footer from "./components/newport/Footer/Footer";
-import About from "./components/newport/About/About";
-import Contact from "./components/newport/Contact/Contact";
+import React from 'react'
+import './app.css'
+import About from './Components/About Section/About'
+import Contact from './Components/Contact Section/Contact'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home Section/Home'
+import Navbar from './Components/Navbar/Navbar'
+import Project from './Components/Projects Section/Project'
+import Skills from './Components/Skills Section/Skills'
 
-function App() {
+
+const App = () => {
   return (
-    <Provider>
-        <Header />
-        <About/>
-        <Projects />
-        <Contact/>
-        <Footer />
-    </Provider>
-  );
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Skills/>
+    <Project/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/newport/Footer/Footer.styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const FooterContainer = styled.div`
-height: 300px;
-`;
-
-export default FooterContainer
